82415b4694ea8dbf4e1d5fdf58f9689b7853d7ac
Yuri Dias Pereira <yuristardias@hotmail.com>
2024-05-17 23:07:04 -0300

    Desafio concluído

---
+++ README.md
@@ -0,0 +1,24 @@
+# Desafio Pokedex
+
+Aplicando conhecimentos de Git/GitHub nesse Desafios de Projeto da DIO, além de explorar o mundo Pokémon através do consumo de uma API REST para a criação de uma Pokédex. Para isso, foram aplicados além do JavaScript, todos os fundamentos de desenvolvimento Web aprendidos até aqui. Como recurso adicional, foi implementado carregamento ajax.
+
+## 👨‍💻 Desenvolvedor
+
+<p>
+    <a href="https://github.com/yuridiasp">
+        <img
+          align=left
+          margin=10
+          width=80
+          src="https://avatars.githubusercontent.com/u/81938754?v=4"
+        />
+    </a>
+      <p>
+        <p>&nbsp&nbspYuri Dias<br>
+        &nbsp&nbsp&nbsp
+    </p>
+</p>
+
+<br>
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/yuridiasp/) [![Discord](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://discord.com/channels/@yuridiasp/) [![Facebook](https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white)](https://www.facebook.com/yuri.dias.7739/) [![X](https://img.shields.io/badge/X-000?style=for-the-badge&logo=x)](https://x.com/YuriDias_P) [![Instagram](https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/yuridiasp/) [![E-mail](https://img.shields.io/badge/-Email-000?style=for-the-badge&logo=microsoft-outlook&logoColor=007BFF)](mailto:yuristardias@hotmail.com)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,118 @@
+const pokemonsList = document.querySelector(".pokemons")
+const limite = 20
 
-console.log('Sucesso!')
+let nextPage = `https://pokeapi.co/api/v2/pokemon?limit=${limite}&offset=0`;
+
+function formatNumber(num) {
+    const formattedNum = num.toString().padStart(3, '0')
+    
+    return '#' + formattedNum
+}
+
+function onScroll() {
+    const observeLastPokemon = pokemonsObserver => {
+        const lastPokemon = pokemonsList.lastChild
+        pokemonsObserver.observe(lastPokemon)
+    }
+
+    const pokemonsObserver = new IntersectionObserver(([lastPokemon], observe) => {
+        if (!lastPokemon.isIntersecting) {
+            return
+        }
+
+        loadPagePokemon()
+        observe.unobserve(lastPokemon.target)
+        observeLastPokemon(pokemonsObserver)
+    })
+
+    observeLastPokemon(pokemonsObserver)
+}
+
+function createElementList (pokemon) {
+    const colorsTypes = {
+        water: "#6890f0",
+        fire: "#f05030",
+        grass: "#f05030",
+        electric: "#78c850",
+        psychic: "#f85888",
+        ice: "#98d8d8",
+        dragon: "#7038f8",
+        dark: "#705848",
+        normal: "#a8a878",
+        fight: "#903028",
+        flying: "#a890f0",
+        poison: "#a040a0",
+        ground: "#e0c068",
+        rock: "#b8a038",
+        bug: "#a8b820",
+        ghost: "#705898",
+        steel: "#b8b8d0",
+        "???": "#68a090",
+    }
+
+    const { id, name, sprites, types } = pokemon
+    const listTypes = types.reduce((list, type) => list + `<li class="type" style="background-color: ${colorsTypes[type.type.name]}">${type.type.name}</li>`, "")
+    const li = document.createElement("li")
+    
+    li.classList.add("pokemon")
+
+    const html = `<span class="number">${formatNumber(id)}</span>
+                    <span class="name">${name}</span>
+
+                    <div class="detail">
+                        <ol class="types">
+                            ${listTypes}
+                        </ol>
+
+                        <img src="${sprites.other.dream_world.front_default}"
+                            alt="Bulbasaur">
+                    </div>`
+    
+    li.innerHTML = html
+    pokemonsList.append(li)
+}
+
+function readDataPokemons (pokemons) {
+    pokemons.forEach(pokemon => {
+        createElementList(pokemon)
+    })
+}
+
+function getPokemons (result) {
+    const { next, results } = result
+
+    nextPage = next
+
+    const promises = results.map(result => {
+        return requestJsonPokemon(result.url)
+    })
+
+    return promises
+}
+
+async function requestJsonPokemon (url) {
+    return fetch(url)
+        .then(result => result.json())
+        .then(json => json)
+        .catch(err => {
+            console.log(err)
+        })
+};
+
+async function loadPagePokemon () {
+    return requestJsonPokemon(nextPage)
+        .then(resultado => {
+            return getPokemons(resultado)
+        }).then(async promises => {
+            return Promise.all(promises).then(pokemons => {
+                readDataPokemons(pokemons)
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+loadPagePokemon().then(() => {
+    onScroll()
+})
